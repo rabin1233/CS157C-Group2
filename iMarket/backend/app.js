a3373309097075396a86743ec39bfb6a763eab76
Akshat Bansal <akshat2610@gmail.com>
2021-03-20 09:59:04 -0700

    REST api for users and products

---
+++ iMarket/backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('dotenv/config');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const productsRoute = require('./routes/Products');
+const userRoute = require('./routes/auth');
+app.use('/products', productsRoute);
+app.use('/user', userRoute);
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, ()=>{
+    console.log('connected to DB!');
+})
+
+
+app.get('/', (req, res) => {
+    res.send('We are on home');
+});
+
+
+app.listen(3000);
---
+++ iMarket/backend/routes/auth.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const {registerationValidation, loginValidation} = require('../validation');
+
+
+router.post('/register', async (req, res) => {
+   //validate data before adding user to DB
+   const {error} = registerationValidation(req.body);
+   if (error){
+       return res.status(400).send(error.details[0].message);
+   }
+
+   //check for unique users
+   const emailExists = await User.findOne({email: req.body.email});
+   if (emailExists){
+       return res.status(400).send('Email already exists');
+   }
+
+   //hash password
+   const salt = await bcrypt.genSalt(10);
+   const hashedPw = await bcrypt.hash(req.body.password, salt);
+
+   const user = new User({
+       name: req.body.name,
+       email: req.body.email,
+       password: hashedPw
+   });
+   try{
+       const savedUser = await user.save();
+       res.send({user: user._id});
+   }catch(err){
+       res.status(400).send(err)
+   }
+});
+
+router.post('/login', async (req, res) => {
+   const {error} = loginValidation(req.body);
+   if (error){
+       return res.status(400).send(error.details[0].message);
+   }
+
+   const user = await User.findOne({email: req.body.email});
+   if (!user){
+       return res.status(400).send('Email or password is wrong');
+   }
+   const validPass = await bcrypt.compare(req.body.password, user.password);
+   if (!validPass){
+       return res.status(400).send('Invalid password');
+   }
+
+   res.send('Login successful');
+});
+
+
+module.exports = router;
