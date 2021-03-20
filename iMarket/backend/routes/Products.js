4e8583c01bd93f732836d402d7c1271c11cd3f1b
Akshat Bansal <akshat2610@gmail.com>
2021-03-19 20:26:45 -0700

    Boilerplate stuff

---
+++ iMarket/backend/routes/Products.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Product')
+
+router.get('/', (req, res)=>{
+    res.send('We are on products');
+});
+
+router.post('/', (req, res)=>{
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    post.save()
+    .then(data=>{
+        res.json(data);
+    }   
+    )
+    .catch(err=>{
+        res.json({message: err});
+    })
+})
+
+module.exports = router;
