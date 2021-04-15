73215e6c5b932fe402979c404ecc75f2b8701286
Rabin Gurung <rabin1233@gmail.com>
2021-04-15 02:00:44 -0700

    modified frontend

---
+++ iMarket/frontend/imarket-fe/src/components/auth.js
@@ -0,0 +1,13 @@
+import React, {Component } from 'react';
+
+export default class Auth extends Component {
+    render (){
+        return (
+            <div>
+                <p>
+                    You are Auth page
+                </p>
+            </div>
+        )
+    }
+}
