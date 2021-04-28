f953a55fb7ca1549e6c095d23ad8311d6cbe49a5
prazes <prajesh.shrestha@sjsu.edu>
2021-04-28 14:22:16 -0700

    Login and register frontend

--- iMarket/frontend/imarket-fe/src/App.js
+++ iMarket/frontend/imarket-fe/src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Register from './Register';
+import Register from './components/register/register';
+import Login from './components/login/login';
+import Home from './components/home/home';
 import './App.css';
 
 function App() {
   return (
-  <div>
+  <div className = "App">
       <BrowserRouter>
       
         <div>
           <Switch>   
-              <Route exact path ="/register" component ={Register}/>   
+              <Route exact path ="/" component ={Home}/>   
+              <Route exact path = "/login" component = {Login}/>
+              <Route exact path = "/register" component = {Register}/>
+
           </Switch>
           </div>
           
